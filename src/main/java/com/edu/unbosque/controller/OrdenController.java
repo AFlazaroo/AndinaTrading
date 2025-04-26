7d0053e6e3ea226591befa118f8f969c92272fac
AFlazaroo <aflazaro@unbosque.edu.co>
2025-04-25 21:18:16 -0500

    Cambioos Yeferson

---
+++ src/main/java/com/edu/unbosque/controller/OrdenController.java
@@ -0,0 +1,12 @@
+package com.edu.unbosque.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrdenController {
+
+    
+
+}
