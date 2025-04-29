534bec9f05fe07733c205ba71d862be9d6eadd24
AndresP2Proyectos <acuta@unbosque.edu.co>
2025-04-28 22:39:59 -0500

    Modificaciones a clases relacionadas con usuarios

--- src/main/java/com/edu/unbosque/service/UsuarioService.java
+++ src/main/java/com/edu/unbosque/service/UsuarioService.java
@@ -3,12 +3,17 @@ package com.edu.unbosque.service;
 import com.edu.unbosque.model.Usuario;
 import com.edu.unbosque.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
 
 
     private UsuarioRepository usuarioRepository;
@@ -30,4 +35,16 @@ public class UsuarioService {
             return Optional.empty();
         }
     }
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return User.builder()
+                .username(usuario.getNombre())
+                .password(usuario.getPassword())
+                .roles(usuario.getRol())
+                .build();
+    }
+
 }
