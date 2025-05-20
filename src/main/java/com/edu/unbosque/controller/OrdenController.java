09b0a84718208bae1b258568683949fff0aedabf
AndresP2Proyectos <acuta@unbosque.edu.co>
2025-05-20 00:38:27 -0500

    Nuevos cambios para venta de acciones

--- src/main/java/com/edu/unbosque/controller/OrdenController.java
+++ src/main/java/com/edu/unbosque/controller/OrdenController.java
@@ -2,6 +2,7 @@ package com.edu.unbosque.controller;
 
 import com.edu.unbosque.model.Orden;
 import com.edu.unbosque.model.Usuario;
+import com.edu.unbosque.service.AlpacaService;
 import com.edu.unbosque.service.OrdenService;
 import com.edu.unbosque.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,12 @@ import java.util.Optional;
 public class OrdenController {
 
     private final OrdenService ordenService;
+    private final AlpacaService alpacaService;
 
     @Autowired
-    public OrdenController(OrdenService ordenService ) {
+    public OrdenController(OrdenService ordenService, AlpacaService alpacaService) {
         this.ordenService = ordenService;
+        this.alpacaService = alpacaService;
     }
 
     @PostMapping("/market")
@@ -29,6 +32,22 @@ public class OrdenController {
     ) {
         try {
             String result = ordenService.placeMarketOrder(symbol, qty, side);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al ejecutar orden de mercado: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/market/sell")
+    public ResponseEntity<String> placeMarketOrder(
+            @RequestParam String symbol,
+            @RequestParam int qty,
+            @RequestParam String side,
+            @RequestParam String time
+    ) {
+        try {
+            String result = ordenService.placeMarketOrder(symbol, qty, side,time);
             return ResponseEntity.ok(result);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
