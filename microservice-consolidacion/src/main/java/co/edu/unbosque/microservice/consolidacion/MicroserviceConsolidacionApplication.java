38f056779027e4e0d84db3da1b5341706e071958
AFlazaroo <aflazaro@unbosque.edu.co>
2025-10-16 11:08:01 -0500

    solucion errores

--- microservice-consolidacion/src/main/java/co/edu/unbosque/microservice/consolidacion/MicroserviceConsolidacionApplication.java
+++ microservice-consolidacion/src/main/java/co/edu/unbosque/microservice/consolidacion/MicroserviceConsolidacionApplication.java
@@ -2,7 +2,6 @@ package co.edu.unbosque.microservice.consolidacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@enableEurekaClient
 @SpringBootApplication
 public class MicroserviceConsolidacionApplication {
 
--- microservice-reporte/src/main/java/co/edu/unbosque/microservice/reporte/MicroserviceReporteApplication.java
+++ microservice-reporte/src/main/java/co/edu/unbosque/microservice/reporte/MicroserviceReporteApplication.java
@@ -2,7 +2,6 @@ package co.edu.unbosque.microservice.reporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@enableEurekaClient
 @SpringBootApplication
 public class MicroserviceReporteApplication {
 
